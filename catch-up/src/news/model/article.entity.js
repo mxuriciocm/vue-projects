1487ddf107d22734f261f1c61041f95593c7370b
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-04-16 02:03:13 -0500

    feat(news-visualization): implemented news article entity

---
+++ catch-up/src/news/model/article.entity.js
@@ -0,0 +1,9 @@
+export class ArticleEntity {
+    constructor(source, title, description, url, urlToImage) {
+        this.source = source;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+    }
+}
